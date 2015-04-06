e8713e578f44b6bf664b3f54a1c75f25b00e5599
langdon <langdon@fedoraproject.org>
2015-04-06 12:12:43 -0400

    getting pretty comfortable with this; modified to pull the orion code from the web; added a conf to fix allowedPaths; modifed the ini file in the Dockerfile to use the correct workspace

---
+++ needs_work/eclipse-orion/build_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker build --rm -t langdon/eclipse-orion .
+
+DOCKER_ID=$(docker run --privileged -d -p 8080:8080 \
+       -v /mnt/host-projects/:/host-files \
+       -v /mnt/host-projects/eclipse-orion-local/workspace:/orion-serverworkspace \
+    	langdon/eclipse-orion)
+
+echo ID = $DOCKER_ID
+
+docker logs $DOCKER_ID
+
